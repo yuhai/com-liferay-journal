brianchandotcom/liferay-portal

LPS-64056 Apply everywhere else

modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/messaging/CheckArticleMessageListener.java
@@ -14,10 +14,9 @@
 
 package com.liferay.journal.web.messaging;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.web.configuration.JournalWebConfiguration;
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
@@ -51,7 +50,7 @@ public class CheckArticleMessageListener
 	@Activate
 	protected void activate(Map<String, Object> properties) {
 		JournalWebConfiguration journalWebConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				JournalWebConfiguration.class, properties);
 
 		schedulerEntryImpl.setTrigger(

modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/JournalPortlet.java
@@ -14,8 +14,6 @@
 
 package com.liferay.journal.web.portlet;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.asset.kernel.exception.AssetCategoryException;
 import com.liferay.asset.kernel.exception.AssetTagException;
 import com.liferay.document.library.kernel.exception.DuplicateFileEntryException;
@@ -66,6 +64,7 @@ import com.liferay.journal.util.impl.JournalUtil;
 import com.liferay.journal.web.asset.JournalArticleAssetRenderer;
 import com.liferay.journal.web.configuration.JournalWebConfiguration;
 import com.liferay.journal.web.portlet.action.ActionUtil;
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.diff.CompareVersionsException;
 import com.liferay.portal.kernel.exception.LocaleException;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -925,7 +924,7 @@ public class JournalPortlet extends MVCPortlet {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_journalWebConfiguration = Configurable.createConfigurable(
+		_journalWebConfiguration = ConfigurableUtil.createConfigurable(
 			JournalWebConfiguration.class, properties);
 	}
 

modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/action/JournalConfigurationAction.java
@@ -14,12 +14,11 @@
 
 package com.liferay.journal.web.portlet.action;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.journal.configuration.JournalGroupServiceConfiguration;
 import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.web.configuration.JournalWebConfiguration;
 import com.liferay.journal.web.display.context.util.JournalWebRequestHelper;
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
@@ -184,7 +183,7 @@ public class JournalConfigurationAction
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_journalWebConfiguration = Configurable.createConfigurable(
+		_journalWebConfiguration = ConfigurableUtil.createConfigurable(
 			JournalWebConfiguration.class, properties);
 	}
 

yuhai/com-liferay-journal

journal-web/src/main/java/com/liferay/journal/web/messaging/CheckArticleMessageListener.java
@@ -14,8 +14,7 @@
 
 package com.liferay.journal.web.messaging;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.web.configuration.JournalWebConfiguration;
 import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
@@ -51,7 +50,7 @@ public class CheckArticleMessageListener
 	@Activate
 	protected void activate(Map<String, Object> properties) {
 		JournalWebConfiguration journalWebConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				JournalWebConfiguration.class, properties);
 
 		schedulerEntryImpl.setTrigger(

journal-web/src/main/java/com/liferay/journal/web/portlet/JournalPortlet.java
@@ -14,10 +14,9 @@
 
 package com.liferay.journal.web.portlet;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
 import com.liferay.asset.kernel.exception.AssetCategoryException;
 import com.liferay.asset.kernel.exception.AssetTagException;
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.document.library.kernel.exception.DuplicateFileEntryException;
 import com.liferay.document.library.kernel.exception.FileSizeException;
 import com.liferay.dynamic.data.mapping.exception.NoSuchStructureException;
@@ -925,7 +924,7 @@ public class JournalPortlet extends MVCPortlet {
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_journalWebConfiguration = Configurable.createConfigurable(
+		_journalWebConfiguration = ConfigurableUtil.createConfigurable(
 			JournalWebConfiguration.class, properties);
 	}
 

journal-web/src/main/java/com/liferay/journal/web/portlet/action/JournalConfigurationAction.java
@@ -14,8 +14,7 @@
 
 package com.liferay.journal.web.portlet.action;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.journal.configuration.JournalGroupServiceConfiguration;
 import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.web.configuration.JournalWebConfiguration;
@@ -184,7 +183,7 @@ public class JournalConfigurationAction
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
-		_journalWebConfiguration = Configurable.createConfigurable(
+		_journalWebConfiguration = ConfigurableUtil.createConfigurable(
 			JournalWebConfiguration.class, properties);
 	}
 